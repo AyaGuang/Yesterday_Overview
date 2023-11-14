093f39dc9584eb709cea2623e21d012fcc19c993
AyaHikari <13685063412@163.com>
2023-11-14 16:21:08 +0800

    v0.3
    不再要求消息简介

--- lwg/interface-v3.py
+++ lwg/interface-v3.py
@@ -3,8 +3,7 @@ class all:
     title = str     #标题
     PageView = int  #浏览量
     appType = int   #平台对应的下标，如抖音：1 百度：2等等
-    brief = str     #简介,如果没有就上传空字符串
-    date = str      #数据日期时间，用于历史记录功能
+
     src = str       #跳转链接
     Redirect = int  #跳转方式,1跳转浏览器；2跳转网页。
     rank = int      #本日在本平台的排名
@@ -12,8 +11,6 @@ class all:
 # 视频类应该额外上传的数据
 class video:
     cover = str     #封面链接
-    #以下内容如果对应平台没有，请写空字符串
-    duration = int  #视频总时长
     up = str        #视频制作者
 
 # 图文类应该额外上传的数据
