43a4475f3cf5ba4ca95731f70558a625c0041b11
linhongyu <87256381+linhongyu0090@users.noreply.github.com>
2023-11-24 14:17:01 +0800

    get_zhihu

---
+++ lhy/get_zhihu.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'http://123.57.208.119:5001/data/toutiao'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    # 在这里处理返回的数据
+    print(data)
+else:
+    # 处理请求失败的情况
+    print('请求失败')                       
